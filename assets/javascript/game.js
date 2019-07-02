06a58f6cc09f0cd05ecabd2a97c71839298aac5e
Myles Carey <seanmyles@comcast.net>
2019-07-02 12:14:59 -0700

    more objects/methods stubbed out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,9 @@
     // Lizard).
 var validTurnChoices = ["r", "p", "s","k","l"];
 
+// ----------------------------------------------------------
 // object for session
+// ----------------------------------------------------------
 var session =  {
   playerName: "",
   wins: 0,
@@ -36,11 +38,20 @@ var session =  {
 
 };
 
-
+// ----------------------------------------------------------
 // object for pool of word
+// ----------------------------------------------------------
 var wordPool = {
-  masterWordList: ['POLK','CARTER','REAGAN','LINCOLN'],
-  availableWords: ['POLK','CARTER','REAGAN','LINCOLN'],
+  masterWordList: ["WASHINGTON","JOHNADAMS","JEFFERSON","MADISON","MONROE","JOHNQADAMS","JACKSON","VAN BUREN","WILLIAMHARRISON",
+  "TYLER","POLK","TAYLOR","FILLMORE","PIERCE","BUCHANAN","LINCOLN","ANDREWJOHNSON","GRANT","HAYES","GARFIELD", 
+  "ARTHUR","CLEVELAND","BENJAMINHARRISON","MCKINLEY","ROOSEVELT","TAFT","WILSON","HARDING","COOLIDGE","HOOVER",
+  "ROOSEVELT","TRUMAN","EISENHOWER","KENNEDY","LYNDONJOHNSON","NIXON","FORD","CARTER","REAGAN","GEORGEBUSH",
+  "CLINTON","GEORGEBUSH","OBAMA","TRUMP"],
+  availableWords: ["WASHINGTON","JOHNADAMS","JEFFERSON","MADISON","MONROE","JOHNQADAMS","JACKSON","VAN BUREN","WILLIAMHARRISON",
+  "TYLER","POLK","TAYLOR","FILLMORE","PIERCE","BUCHANAN","LINCOLN","ANDREWJOHNSON","GRANT","HAYES","GARFIELD", 
+  "ARTHUR","CLEVELAND","BENJAMINHARRISON","MCKINLEY","ROOSEVELT","TAFT","WILSON","HARDING","COOLIDGE","HOOVER",
+  "ROOSEVELT","TRUMAN","EISENHOWER","KENNEDY","LYNDONJOHNSON","NIXON","FORD","CARTER","REAGAN","GEORGEBUSH",
+  "CLINTON","GEORGEBUSH","OBAMA","TRUMP"],
 
   // is word available 
   isWordAvailable: function() {
@@ -61,12 +72,13 @@ var wordPool = {
   }
 };
 
+// ----------------------------------------------------------
 // object for user interface - i.e. the html page elements
-
-
+// ----------------------------------------------------------
 var userInterface = {
 // properties to be determined later - need more understanding how
 // on page elements will be manipulated during game play first
+  usedLetters: [],
 
   // clear word element
   clearWordElement: function() {
@@ -76,25 +88,69 @@ var userInterface = {
     // due to time constraints i might have a fixed number of letter
     // elements or maybe i will have a "diplay word" that has dashes for
     // unreveiled letters - TBD
-    },
+  },
 
   // clear picked letter element
-  clearPickedLetters: function() {
-    console.log("in userInterface.clearPickedLetters");  
-    // this is were the on page element of picked letters will be reset
-    // how this looks is TBD
+  clearUsedLetters: function() {
+    console.log("in userInterface.clearUsedLetters");  
+    this.usedLetters.splice(0,this.usedLetters.length);
+    // this is where the on page element of picked letters will be reset
+    // how this looks is TBD      
+    },
+   
 
+  // add a letter to the picked letter list
+  // starting to think this belongs in the game object 
+  // and should be replaced here with just a method
+  // that updates what word is shown on screen - not sure yet
+  addLetterToUsedList: function(letter) {
+    console.log("in userInterface.addLetterToUsedList"); 
+    this.usedLetters.push(letter); 
+    // this is where list of letters already used will be added to 
+    // caller sends one letter to add to list
+  },  
+
+  // find all occurance of letter in game word and reveal it on page
+  // starting to think this belongs in the game object 
+  // and should be replaced here with just a method
+  // that updates what word is shown on screen - not sure yet
+  showLetter: function(letter) {
+    console.log("in userInterface.showLetter"); 
+    // this is where letter in the on page word element will be 
+    // changed to be visable - function will have to find all
+    // ocuurances in the word 
+    // caller sends one letter to add to list
   }
+};
 
 
-
-}
+// ----------------------------------------------------------
+// object for game
+// ----------------------------------------------------------
+// this will have much of the core logic 
+// have to see how it fills out - some methods in userInterface
+// might be better placed here - such as managing the word state
+// and managing the used letter list
+// perhaps that happens here and userInterface simply has to act
+// on whole word strings that it is passed for the word and for
+// the used leter list - maybe
+var game = {
+  // properties to be determined later - need more understanding how
+  // on page elements will be manipulated during game play first
+    
+    // placeholder
+    placeholder: function() {
+      console.log("in game.placeholder");  
+    }
+  }
+  
 
 
 // -------------------------------------------------------------------
 //  *** Start of game flow *** 
 // -------------------------------------------------------------------
 session.beginSession();
+game.placeholder();
 
 wordPool.masterWordList.forEach(element => {
   console.log(element);
@@ -107,7 +163,26 @@ console.log("words remaining: " + wordPool.isWordAvailable());
 // if appropriate call location is not yet known
 wordPool.getWordFromPool();
 userInterface.clearWordElement();
-userInterface.clearPickedLetters();
+// show used letter list
+userInterface.usedLetters.forEach(element => {
+  console.log(element)
+});
+// add some letters to used letter list
+userInterface.addLetterToUsedList('Q');
+userInterface.addLetterToUsedList('R');
+userInterface.addLetterToUsedList('A');
+// show used letter list
+userInterface.usedLetters.forEach(element => {
+  console.log(element)
+});
+// clear used letter list
+userInterface.clearUsedLetters();
+// show used letter list
+userInterface.usedLetters.forEach(element => {
+  console.log(element)
+});
+
+userInterface.showLetter();
 
 
 // ----------------------------------------------------------
